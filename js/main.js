7bd11b55fe3ee87281fd0886e4f9c9003fa94a97
vortispy <onepiece.no23@gmail.com>
2017-01-13 17:57:47 +0900

    create memo edit page.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
+const editpage = {
+	template:
+		'<div>'+
+		'<input type="text" v-model="title"/><textarea v-model="memo"/>'+
+		'<button v-on:click="saveMemo">save</button>'+
+		'</div>',
+	data: function (){
+		return {
+			title: '',
+			memo: ''
+		};
+	},
+	methods: {
+		saveMemo: function () {
+			return
+		}
+	}
+};
+const routes = [
+	{ path: '/create', component: editpage }
+];
+const router = new VueRouter({
+	routes
+});
 const app = new Vue({
+	router,
 	el: '#app',
 	data: {
 		message: 'hello'
