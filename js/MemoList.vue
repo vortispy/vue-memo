db8869b1daf98151b92067542a17957da9637091
vortispy <onepiece.no23@gmail.com>
2017-02-06 11:43:26 +0900

    first top design.

--- js/MemoList.vue
+++ js/MemoList.vue
@@ -1,13 +1,13 @@
 <template>
-	<div>
-		<span v-if="memoStore.memos.length === 0">You don't have any memos.</span>
-		<ul v-else>
-			<li v-for="memo in memoStore.memos">
-				<router-link :to="{ name: 'update', params: { uid: memo.uid }}">
+	<div class="">
+		<span v-if="memoStore.memos.length === 0" class="col-xs-12">You don't have any memos.</span>
+		<ul v-else class="list-group">
+			<li v-for="memo in memoStore.memos" class="col-xs-12 list-group-item">
+				<router-link :to="{ name: 'update', params: { uid: memo.uid }}" class="col-xs-8 text-left">
 					{{ memo.title }}
 				</router-link>
-				<span>{{ memo.created_at }}</span>
-				<button v-on:click="deleteMemo(memo.uid)">delete</button>
+				<span class="col-xs-2">{{ memo.created_at }}</span>
+				<button v-on:click="deleteMemo(memo.uid)" class="col-xs-offset-1">X</button>
 			</li>
 		</ul>
 	</div>
